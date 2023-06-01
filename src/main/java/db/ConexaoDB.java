4965a2297eaf12f16b493baeede99035e453d06a
Renato <renatodepsf@gmail.com>
2023-05-31 22:21:35 -0300

    conectando no db

--- src/main/java/db/ConexaoDB.java
+++ src/main/java/db/ConexaoDB.java
@@ -1,6 +1,5 @@
 package db;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,17 +13,22 @@ public class ConexaoDB {
     private static Connection conexao = null;
 
     public static Connection getConectar() {
-
         if (conexao == null) {
-//            String url = "jdbc:sqlserver://;serverName=RENATO;databaseName=Kadoshi;integratedSecurity=true";
             try {
-                conexao = DriverManager.getConnection(getProps().getProperty("url"));
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                Properties props = getProps();
+                conexao = DriverManager.getConnection(props.getProperty("url"), props.getProperty("usuario"),
+                        props.getProperty("senha"));
             } catch (SQLException e) {
+                e.printStackTrace();
                 throw new DbException(e.getMessage());
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
             }
         }
         return conexao;
     }
+
 
     public static void getDesconectar() {
         if(conexao != null) {
@@ -38,13 +42,11 @@ public class ConexaoDB {
 
     private static Properties getProps() {
         Properties props = new Properties();
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream("config.properties");
-        } catch (FileNotFoundException e) {
-            throw new DbException(e.getMessage());
-        }
-        try {
+        try (InputStream inputStream = ConexaoDB.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Arquivo config.properties não encontrado no classpath");
+            }
             props.load(inputStream);
         } catch (IOException e) {
             throw new DbException(e.getMessage());
